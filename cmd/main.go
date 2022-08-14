be7bc4a348bbac7ffc8f6142f2e32fc0b36d19d2
jszafran <jszafran.pv@gmail.com>
2022-08-14 15:51:50 +0200

    Initial working version.

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
-	sqlcomm "github.com/jszafran/go-sql-commas"
-	"golang.design/x/clipboard"
+	"flag"
+	go_sql_commas "github.com/jszafran/go-sql-commas"
 )
 
 func main() {
-	clpText := sqlcomm.ReadClipboard()
-	spl := sqlcomm.SplitContent(clpText)
-	res := sqlcomm.WrapCommas(spl)
-	clipboard.Write(sqlcomm.ClipboardFormat, res)
+	var wrapStrings bool
+	flag.BoolVar(&wrapStrings, "strings", false, "Wraps output with single quotes (SQL strings).")
+	flag.Parse()
+
+	clp := go_sql_commas.SystemClipboard{}
+
+	var wrapped []byte
+	if wrapStrings {
+		wrapped = go_sql_commas.HandleStrings(clp)
+	} else {
+		wrapped = go_sql_commas.HandleNumbers(clp)
+	}
+
+	go_sql_commas.PasteToClipboard(wrapped)
 }
--- sql_commas.go
+++ sql_commas.go
@@ -1,22 +1,64 @@
 package go_sql_commas
 
 import (
+	"fmt"
 	"golang.design/x/clipboard"
 	"strings"
 )
 
 const ClipboardFormat = clipboard.FmtText
 
-func ReadClipboard() string {
-	clpb := clipboard.Read(ClipboardFormat)
-	return string(clpb)
+type Clipboard interface {
+	Read() string
 }
 
-func SplitContent(s string) []string {
-	// start with most naive implementation
+type FakeClipboard struct {
+	Data string
+}
+
+func (fc FakeClipboard) Read() string {
+	return fc.Data
+}
+
+type SystemClipboard struct{}
+
+func (sc SystemClipboard) Read() string {
+	return string(clipboard.Read(ClipboardFormat))
+}
+
+// splitContent split clipboard string into records based on separator character
+func splitContent(s string) []string {
+	// start with most naive implementation - assume \n is the separator
 	return strings.Split(s, "\n")
 }
 
-func WrapCommas(recs []string) []byte {
+// addTrailingComma adds trailing comma to records (except for the last one)
+// so they can be later used within SQL IN clause. Returns slice of bytes.
+func addTrailingComma(recs []string) []byte {
 	return []byte(strings.Join(recs, ",\n"))
+}
+
+func addSingleQuotes(recs []string) []string {
+	r := make([]string, len(recs))
+	for i, rec := range recs {
+		r[i] = fmt.Sprintf("'%s'", strings.Replace(rec, "'", "''", -1))
+	}
+	return r
+}
+
+func HandleNumbers(clp Clipboard) []byte {
+	d := clp.Read()
+	recs := splitContent(d)
+	return addTrailingComma(recs)
+}
+
+func HandleStrings(clp Clipboard) []byte {
+	d := clp.Read()
+	recs := splitContent(d)
+	quoted := addSingleQuotes(recs)
+	return addTrailingComma(quoted)
+}
+
+func PasteToClipboard(b []byte) {
+	clipboard.Write(ClipboardFormat, b)
 }
