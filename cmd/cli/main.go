7805ed10ef2db86c0f41bc3d5989465b0e4835b2
jszafran <jszafran.pv@gmail.com>
2022-08-16 21:39:37 +0200

    Correct flags description.

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -8,9 +8,9 @@ import (
 func main() {
 	var forStrings bool
 	var leadingCommas bool
-	flag.BoolVar(&forStrings, "strings", false, "Wraps rows with single quotes (SQL strings).")
+	flag.BoolVar(&forStrings, "strings", false, "Wraps rows with single quotes (for SQL strings).")
 	flag.BoolVar(&leadingCommas, "leading_commas", false, "Use leading commas for separating rows "+
-		"(trailing commas are used by default.)")
+		"(trailing commas used by default).")
 	flag.Parse()
 
 	clp := go_sql_commas.SystemClipboard{}
