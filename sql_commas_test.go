65ec811128fc67875eea1bcb70766c6ef773f403
jszafran <jszafran.pv@gmail.com>
2022-08-16 21:32:33 +0200

    Split longer lines in tests.

--- sql_commas_test.go
+++ sql_commas_test.go
@@ -31,11 +31,23 @@ func TestHandleNumbers(t *testing.T) {
 		{"1", "1", true},
 		{fmt.Sprintf("1%s2", LineBreak), fmt.Sprintf("1,%s2", LineBreak), false},
 		{fmt.Sprintf("1%s2", LineBreak), fmt.Sprintf("1%s,2", LineBreak), true},
-		{fmt.Sprintf("1%s2%s3", LineBreak, LineBreak), fmt.Sprintf("1,%s2,%s3", LineBreak, LineBreak), false},
-		{fmt.Sprintf("1%s2%s3", LineBreak, LineBreak), fmt.Sprintf("1%s,2%s,3", LineBreak, LineBreak), true},
+		{
+			fmt.Sprintf("1%s2%s3", LineBreak, LineBreak),
+			fmt.Sprintf("1,%s2,%s3", LineBreak, LineBreak), false,
+		},
+		{
+			fmt.Sprintf("1%s2%s3", LineBreak, LineBreak),
+			fmt.Sprintf("1%s,2%s,3", LineBreak, LineBreak), true,
+		},
 		//Excel adding extra line break at the end edge case
-		{fmt.Sprintf("1%s2%s", LineBreak, LineBreak), fmt.Sprintf("1,%s2", LineBreak), false},
-		{fmt.Sprintf("1%s2%s", LineBreak, LineBreak), fmt.Sprintf("1%s,2", LineBreak), true},
+		{
+			fmt.Sprintf("1%s2%s", LineBreak, LineBreak),
+			fmt.Sprintf("1,%s2", LineBreak), false,
+		},
+		{
+			fmt.Sprintf("1%s2%s", LineBreak, LineBreak),
+			fmt.Sprintf("1%s,2", LineBreak), true,
+		},
 	}
 	for _, c := range cases {
 		clp := FakeClipboard{c.input}
@@ -52,15 +64,39 @@ func TestHandleStrings(t *testing.T) {
 	cases := []testCase{
 		{"a", "'a'", false},
 		{"a", "'a'", false},
-		{fmt.Sprintf("a%sb", LineBreak), fmt.Sprintf("'a',%s'b'", LineBreak), false},
-		{fmt.Sprintf("a%sb", LineBreak), fmt.Sprintf("'a'%s,'b'", LineBreak), true},
-		{fmt.Sprintf("a%sb%sc", LineBreak, LineBreak), fmt.Sprintf("'a',%s'b',%s'c'", LineBreak, LineBreak), false},
-		{fmt.Sprintf("a%sb%sc", LineBreak, LineBreak), fmt.Sprintf("'a'%s,'b'%s,'c'", LineBreak, LineBreak), true},
-		{fmt.Sprintf("'a'%sb%s'c''", LineBreak, LineBreak), fmt.Sprintf("'''a''',%s'b',%s'''c'''''", LineBreak, LineBreak), false},
-		{fmt.Sprintf("'a'%sb%s'c''", LineBreak, LineBreak), fmt.Sprintf("'''a'''%s,'b'%s,'''c'''''", LineBreak, LineBreak), true},
+		{
+			fmt.Sprintf("a%sb", LineBreak),
+			fmt.Sprintf("'a',%s'b'", LineBreak), false,
+		},
+		{
+			fmt.Sprintf("a%sb", LineBreak),
+			fmt.Sprintf("'a'%s,'b'", LineBreak), true,
+		},
+		{
+			fmt.Sprintf("a%sb%sc", LineBreak, LineBreak),
+			fmt.Sprintf("'a',%s'b',%s'c'", LineBreak, LineBreak), false,
+		},
+		{
+			fmt.Sprintf("a%sb%sc", LineBreak, LineBreak),
+			fmt.Sprintf("'a'%s,'b'%s,'c'", LineBreak, LineBreak), true,
+		},
+		{
+			fmt.Sprintf("'a'%sb%s'c''", LineBreak, LineBreak),
+			fmt.Sprintf("'''a''',%s'b',%s'''c'''''", LineBreak, LineBreak), false,
+		},
+		{
+			fmt.Sprintf("'a'%sb%s'c''", LineBreak, LineBreak),
+			fmt.Sprintf("'''a'''%s,'b'%s,'''c'''''", LineBreak, LineBreak), true,
+		},
 		// Excel adding extra line break at the end edge case
-		{fmt.Sprintf("1'%s'2%s", LineBreak, LineBreak), fmt.Sprintf("'1''',%s'''2'", LineBreak), false},
-		{fmt.Sprintf("1'%s'2%s", LineBreak, LineBreak), fmt.Sprintf("'1'''%s,'''2'", LineBreak), true},
+		{
+			fmt.Sprintf("1'%s'2%s", LineBreak, LineBreak),
+			fmt.Sprintf("'1''',%s'''2'", LineBreak), false,
+		},
+		{
+			fmt.Sprintf("1'%s'2%s", LineBreak, LineBreak),
+			fmt.Sprintf("'1'''%s,'''2'", LineBreak), true,
+		},
 	}
 	for _, c := range cases {
 		clp := FakeClipboard{c.input}
